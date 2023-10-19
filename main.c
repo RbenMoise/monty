35f4a5d872cb72a6962161520a0480ca07e49b04
RbenMoise <reubenmoses91@gmail.com>
2023-10-19 13:20:30 -0700

    all

--- main.c
+++ main.c
@@ -1,86 +1,83 @@
-#define _POSIX_C_SOURCE 200809L
 #include "monty.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-
-global_t vglo;
+stack_t *head = NULL;
 
 /**
- * free_vglo - Function to free the global structure
+ * main - entry point
+ * @argc: arguments count
+ * @argv: list of arguments
+ * Return: always 0
  */
-void free_vglo(void)
-{
-    if (vglo.head != NULL)
-        free_dlistint(vglo.head);
-    if (vglo.buffer != NULL)
-        free(vglo.buffer);
-    if (vglo.fd != NULL)
-        fclose(vglo.fd);
-}
 
-/**
- * start_vglo - Function to initialize the global structure
- * @fd: File descriptor
- */
-void start_vglo(FILE *fd)
-{
-    vglo.lifo = 1;
-    vglo.cont = 1;
-    vglo.arg = NULL;
-    vglo.head = NULL;
-    vglo.fd = fd;
-    vglo.buffer = NULL;
-}
-
-/**
- * main - Monty ByteCode Interpreter
- * @argc: Number of arguments
- * @argv: Array of arguments
- * Return: Always 0
- */
 int main(int argc, char *argv[])
 {
-    FILE *fd;
-    size_t size = 0;
-    ssize_t nlines = 0;
-    void (*f)(stack_t **stack, unsigned int line_number);
-    char *lines[2];
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	open_file(argv[1]);
+	free_nodes();
+	return (0);
+}
 
-    if (argc != 2)
-    {
+/**
+ * create_node - Creates a node.
+ * @n: Number to go inside the node.
+ * Return: Upon sucess a pointer to the node. Otherwise NULL.
+ */
+stack_t *create_node(int n)
+{
+	stack_t *node;
 
-    }
+	node = malloc(sizeof(stack_t));
+	if (node == NULL)
+		err(4);
+	node->next = NULL;
+	node->prev = NULL;
+	node->n = n;
+	return (node);
+}
 
-    fd = fopen(argv[1], "r");
-    if (fd == NULL)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
+/**
+ * free_nodes - Frees nodes in the stack.
+ */
+void free_nodes(void)
+{
+	stack_t *tmp;
 
-    start_vglo(fd);
+	if (head == NULL)
+		return;
 
-    while ((nlines = getline(&vglo.buffer, &size, fd)) != -1)
-    {
-        lines[0] = _strtoky(vglo.buffer, " \t\n");
-        if (lines[0] == NULL)
-            continue;
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
 
-        f = get_opcodes(lines[0]);
-        if (f == NULL)
-        {
-            fprintf(stderr, "L%u: unknown instruction %s\n", vglo.cont, lines[0]);
-            free_vglo();
-            exit(EXIT_FAILURE);
-        }
 
-        vglo.arg = _strtoky(NULL, " \t\n");
-        f(&vglo.head, vglo.cont);
-        vglo.cont++;
-    }
+/**
+ * add_to_queue - Adds a node to the queue.
+ * @new_node: Pointer to the new node.
+ * @ln: line number of the opcode.
+ */
+void add_to_queue(stack_t **new_node, __attribute__((unused))unsigned int ln)
+{
+	stack_t *tmp;
 
-    free_vglo();
-    exit(EXIT_SUCCESS);
+	if (new_node == NULL || *new_node == NULL)
+		exit(EXIT_FAILURE);
+	if (head == NULL)
+	{
+		head = *new_node;
+		return;
+	}
+	tmp = head;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+
+	tmp->next = *new_node;
+	(*new_node)->prev = tmp;
+
 }
